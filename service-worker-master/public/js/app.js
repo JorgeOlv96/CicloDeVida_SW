85e2e056e04748e658154f7c14f651bcb4baefe4
JorgeOlv96 <2020171010@uteq.edu.mx>
2024-02-12 21:05:18 -0600

    CicloDeVIda_2

--- service-worker-master/public/js/app.js
+++ service-worker-master/public/js/app.js
@@ -1,3 +1,32 @@
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('/sw.js')
+        .then(reg => {
+            Notification.requestPermission().then(permission => {
+                if (permission === 'granted') {
+                    reg.showNotification('Hola Mundo');
+                }
+            });
+        }).catch(err => {
+            console.error('Service Worker not registered', err)
+        });
 }
+
+// if (navigator.serviceWorker) {
+//     navigator.serviceWorker.register('/sw.js')
+//         .then(reg => {
+//             setTimeout(() => {
+//                 reg.sync.register('enviar-fotos');
+//                 console.log('Se enviaron fotos', reg);
+//             }, 3000);
+//         }).catch(err => {
+//             console.error('Service Worker not registered', err)
+        // });
+    // .then(reg => {
+    //     console.log('Service Worker registered', reg)
+    // }).catch(err => {
+    //     console.error('Service Worker not registered', err)
+    // });
+// }
+// fetch('https://fakestoreapi.com/products/1')
+//     .then(resp => resp.json())
+//     .then(console.log);
